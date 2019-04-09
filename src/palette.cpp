05ac622af657ef3571a9d3caae06c34fdfd964d3
Arnaud Gatignol <arnaud.gatignol@thalesgroup.com>
2019-04-09 17:48:05 +0200

    Add colour palette

--- src/palette.cpp
+++ src/palette.cpp
@@ -1,1 +1,40 @@
 #include "palette.hpp"
+
+
+Idatag_palette::Idatag_palette(std::vector<std::string>* feeders)
+{
+	this->feeders = feeders;
+	QColor colour;
+	uint hue = 0;
+	uint saturation = 128;
+	uint value = 180;
+	uint alpha = 255;
+
+	srand(time(NULL));
+
+	for (const auto & feeder : *feeders) {
+		if (hue > 359) {
+			colour = colour.fromHsv(rand() % 359, rand() % 240 + 128, value, alpha);
+			this->association.insert(std::pair<std::string, QColor>(feeder, colour));
+		}
+		else {
+			colour = colour.fromHsv(hue, saturation, value, alpha);
+			this->association.insert(std::pair<std::string, QColor>(feeder, colour));
+			hue += 30;
+		}
+	}
+}
+
+void Idatag_palette::add_feeder_colour(std::string& feeder)
+{
+	QColor colour;
+	uint value = 180;
+	uint alpha = 255;
+	colour = colour.fromHsv(rand() % 359, rand() % 240 + 128, value, alpha);
+	this->association.insert(std::pair<std::string, QColor>(feeder, colour));
+}
+
+QColor Idatag_palette::get_feeder_colour(std::string& feeder)
+{
+	return this->association[feeder];
+}
--- src/palette.hpp
+++ src/palette.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
-class idatag_palette {
+#include "QtGui/qcolor.h"
 
+#include <time.h>
+#include <stdlib.h>
 
+class Idatag_palette {
+private:
+	std::map<std::string, QColor> association;
+	std::vector<QColor> colours;
+	std::vector<std::string>* feeders;
+public:
+	Idatag_palette(std::vector<std::string>*);
+	void add_feeder_colour(std::string&);
+	QColor	get_feeder_colour(std::string&);
 };

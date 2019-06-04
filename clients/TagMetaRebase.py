299a24cd7447dfcef275e3af792fb4b2590e6e23
Arnaud Gatignol <arnaud.gatignol@thalesgroup.com>
2019-06-04 13:52:47 +0200

    Add meta client to rebase tag

---
+++ clients/TagMetaRebase.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+def main(base, input, output):
+	with open(input, 'r') as f:
+		fjson = json.load(f)
+		for entry in fjson:
+			entry["offset"] += base
+	with open(output, 'wb') as h:
+		json.dump(fjson, h)
+
+def usage():
+	print("Usage : TagMetaRebase.py <base> <input tag file> <output tag file>")
+		
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		usage()
+		exit(0)
+	main(sys.argv[1], sys.argv[2], sys.argv[3])

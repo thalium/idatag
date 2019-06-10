dca8aa17fc33c02b0b724c1e872e6a593d865c49
Arnaud Gatignol <arnaud.gatignol@thalesgroup.com>
2019-06-10 16:23:27 +0200

    Fix TagMetaRebase.py argues nb check

--- clients/TagMetaRebase.py
+++ clients/TagMetaRebase.py
@@ -6,14 +6,14 @@ def main(base, input, output):
 		fjson = json.load(f)
 		for entry in fjson:
 			entry["offset"] += base
-	with open(output, 'wb') as h:
+	with open(output, 'w') as h:
 		json.dump(fjson, h)
 
 def usage():
 	print("Usage : TagMetaRebase.py <base> <input tag file> <output tag file>")
 		
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
+	if len(sys.argv) != 4:
 		usage()
 		exit(0)
 	main(sys.argv[1], sys.argv[2], sys.argv[3])
